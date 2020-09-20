25f032929803229f576138a8ecc5de7ba76bdd68
Zeye Gu <zeyegu@gmail.com>
2020-09-20 13:37:16 -0400

    ready to be deployed

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const { body } = require('express-validator');
+const path = require('path');
 
 //Connect Database
 connectDB();
 
 const app = express();
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 //Init Middleware
 app.use(express.json({
@@ -18,6 +19,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//Serve static asserts in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 
 const PORT = process.env.PORT || 5000; // run on local port 5000;
